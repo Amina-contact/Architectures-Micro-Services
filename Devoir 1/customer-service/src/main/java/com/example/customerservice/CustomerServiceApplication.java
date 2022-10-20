b2d80ed755b7feba097848fb226f52a4c8683445
amina <dahmouniamina.contact@gmail.com>
2022-10-20 23:32:06 +0100

    First Microservice customer-service

--- Devoir 1/customer-service/src/main/java/com/example/customerservice/CustomerServiceApplication.java
+++ Devoir 1/customer-service/src/main/java/com/example/customerservice/CustomerServiceApplication.java
@@ -1,7 +1,11 @@
 package com.example.customerservice;
 
+import com.example.customerservice.dto.CustomerRequestDTO;
+import com.example.customerservice.services.CustomerService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
@@ -9,5 +13,11 @@ public class CustomerServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
     }
-
+    @Bean
+    CommandLineRunner start(CustomerService customerService){
+        return args -> {
+            customerService.save(new CustomerRequestDTO("A01","Amina","amina@gmail.com"));
+            customerService.save(new CustomerRequestDTO("A02","Fatima","fatima@gmail.com"));
+        };
+    }
 }
